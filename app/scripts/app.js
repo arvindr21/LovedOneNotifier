cecf75c72ed5f68e2d58a15c06dca0e9e631a949
Arvind Ravulavaru <arvind.ravulavaru@gmail.com>
2015-02-01 10:31:49 +0530

    bg service

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,84 +5,93 @@
 angular.module('LovedOneNotifier', ['ionic', 'config', 'LocalStorageModule', 'LovedOneNotifier.controllers'])
 
 .run(function($ionicPlatform, $rootScope, localStorageService, $location, $timeout) {
-  $ionicPlatform.ready(function() {
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      StatusBar.styleDefault();
-    }
+    $ionicPlatform.ready(function() {
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
 
-    var skipIntro;
+        var myService = cordova.require('com.red_folder.phonegap.plugin.backgroundservice.BackgroundService');
 
-    $rootScope.$on('$stateChangeStart',
-      function(event, toState, toParams, fromState) {
+        myService.startService(function() { //r
+                // service started
+            },
+            function(e) {
+                //alert('Error: ' + e.ErrorMessage);
+            });
+
+        var skipIntro;
+
+        $rootScope.$on('$stateChangeStart',
+            function(event, toState, toParams, fromState) {
+                skipIntro = localStorageService.get('skip') === 'true' ? true : false;
+
+                if (fromState.name === 'home' && toState.name === 'intro') {
+                    if (skipIntro) {
+                        navigator.app.exitApp();
+                    }
+                }
+                if (fromState.name === 'intro' && toState.name === 'loading') {
+                    navigator.app.exitApp();
+                }
+                if (fromState.name === 'home' && toState.name === 'loading') {
+                    navigator.app.exitApp();
+                }
+                if (toState.name === 'intro') {
+                    if (skipIntro) {
+                        location.href = '#/home';
+                    }
+                }
+            });
+
         skipIntro = localStorageService.get('skip') === 'true' ? true : false;
 
-        if (fromState.name === 'home' && toState.name === 'intro') {
-          if (skipIntro) {
-            navigator.app.exitApp();
-          }
+        if ($location.$$url === '/loading') {
+            $timeout(function() {
+                if (skipIntro) {
+                    location.href = '#/home';
+                } else {
+                    location.href = '#/intro';
+                }
+            }, 2000);
         }
-        if (fromState.name === 'intro' && toState.name === 'loading') {
-          navigator.app.exitApp();
-        }
-        if (fromState.name === 'home' && toState.name === 'loading') {
-          navigator.app.exitApp();
-        }
-        if (toState.name === 'intro') {
-          if (skipIntro) {
-            location.href = '#/home';
-          }
-        }
-      });
-
-    skipIntro = localStorageService.get('skip') === 'true' ? true : false;
-
-    if ($location.$$url === '/loading') {
-      $timeout(function() {
-        if (skipIntro) {
-          location.href = '#/home';
-        } else {
-          location.href = '#/intro';
-        }
-      }, 2000);
-    }
 
 
-  });
+    });
 })
 
 .config(function($stateProvider, $urlRouterProvider, localStorageServiceProvider) {
 
-  $stateProvider
-    .state('loading', {
-      url: '/loading',
-      templateUrl: 'templates/loading.html'
-    })
+    $stateProvider
+        .state('loading', {
+            url: '/loading',
+            templateUrl: 'templates/loading.html'
+        })
 
     .state('intro', {
-      url: '/intro',
-      templateUrl: 'templates/intro.html',
-      controller: 'IntroCtrl'
+        url: '/intro',
+        templateUrl: 'templates/intro.html',
+        controller: 'IntroCtrl'
     })
 
     .state('home', {
-      url: '/home',
-      templateUrl: 'templates/home.html',
-      controller: 'HomeCtrl'
+        url: '/home',
+        templateUrl: 'templates/home.html',
+        controller: 'HomeCtrl'
     })
 
     .state('contacts', {
-      url: '/contacts',
-      templateUrl: 'templates/contacts.html',
-      controller: 'ContactsCtrl'
+        url: '/contacts',
+        templateUrl: 'templates/contacts.html',
+        controller: 'ContactsCtrl'
     });
 
-  $urlRouterProvider.otherwise('/loading');
+    $urlRouterProvider.otherwise('/loading');
 
-  localStorageServiceProvider
-    .setPrefix('lon');
+    localStorageServiceProvider
+        .setPrefix('lon');
 
 
 });
